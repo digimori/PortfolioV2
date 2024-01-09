2914fba2c8cf7d6c6cbdc13c4329c969522453df
digimori <hanaorikuu@gmail.com>
2024-01-09 14:58:37 +0000

    Initial commit - Dependencies installed - File tree created

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# PortfolioV2
-Portfolio Rebuild using Reactjs
+# Portfolio V2
+
+## Technologies and Libraries used:
+
+- HTML5
+- SaSS
+- JavaScript
+- ReactJS
+- Framer Motion
+
+## Components:
+
+## Snap Scroll:
+
+## Framer Motion notes:
---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Cursor from "./components/cursor/Cursor";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+
+function App() {
+  return (
+    <div>
+      <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Portfolio />
+      </section>
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
+}
+
+export default App;

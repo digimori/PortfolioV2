dc4e5733a17b582a5e441b752bb6ecabfc61a304
digimori <hanaorikuu@gmail.com>
2024-01-11 19:57:09 +0000

    Sidebar navivation animations added.

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,16 +1,48 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const listVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 
 const Links = () => {
   const linkItems = ["Home", "Education", "Projects", "Contact"];
 
   return (
-    <div className="sidebar__links">
+    <motion.div className="sidebar__links" variants={variants}>
       {linkItems.map((link) => (
-        <a href={`#${link}`} key={link}>
+        <motion.a
+          href={`#${link}`}
+          key={link}
+          variants={listVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
           {link}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 

d0c29a68400d0527a0323fd8cf4403618daf54ce
digimori <hanaorikuu@gmail.com>
2024-01-11 20:41:09 +0000

    Basic hero section created. Placeholder image current.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./hero.scss";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero__container">
+      <div className="hero__container-wrapper">
+        <div className="hero__container-text">
+          <h1>Paige Bendall</h1>
+          <h2>Web Developer</h2>
+          <div className="hero__container-text_buttons">
+            <button>Latest Projects</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="#" alt="scroller" />
+        </div>
+      </div>
+      <div className="hero__container-image">
+        <img src="/src/assets/hero.png" alt="heroimage" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;

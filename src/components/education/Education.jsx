21b2f6dbc57ffc066eeb32337d9fe34614e73ba8
digimori <hanaorikuu@gmail.com>
2024-01-13 20:23:25 +0000

    Education and experience section added /incomplete - Framer motion added to hero section

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,7 +1,107 @@
-import React from "react";
+import { useRef } from "react";
+import "./education.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChilden: 0.1,
+    },
+  },
+};
 
 const Education = () => {
-  return <div>Services</div>;
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: "-50px" });
+
+  return (
+    <motion.div
+      className="education__container"
+      variants={variants}
+      initial="initial"
+      ref={ref}
+      animate={isInView && "animate"}
+    >
+      <motion.div className="education__container-text" variants={variants}>
+        <p>
+          Want my education and working background?
+          <br /> This section is for you.
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="education__container-title" variants={variants}>
+        <div className="education__container-title_text">
+          <h2>
+            <motion.b whileHover={{ color: "orange" }}>Education </motion.b> and
+          </h2>
+        </div>
+        <div className="education__container-title_text" variants={variants}>
+          <h2>
+            <motion.b whileHover={{ color: "orange" }}>Experience</motion.b>.
+          </h2>
+        </div>
+      </motion.div>
+      <motion.div className="education__container-list" variants={variants}>
+        <motion.div
+          className="education__container-list_content"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Scottish Tech Army</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            quam vitae eos labore odio incidunt similique dicta possimus id. Hic
+            nesciunt quos commodi non voluptatibus ratione temporibus sint animi
+            at.
+          </p>
+        </motion.div>
+        <motion.div
+          className="education__container-list_content"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Code Institute</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            quam vitae eos labore odio incidunt similique dicta possimus id. Hic
+            nesciunt quos commodi non voluptatibus ratione temporibus sint animi
+            at.
+          </p>
+        </motion.div>
+        <motion.div
+          className="education__container-list_content"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Scottish Tech Army</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            quam vitae eos labore odio incidunt similique dicta possimus id. Hic
+            nesciunt quos commodi non voluptatibus ratione temporibus sint animi
+            at.
+          </p>
+        </motion.div>
+        <motion.div
+          className="education__container-list_content"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Code Institute</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            quam vitae eos labore odio incidunt similique dicta possimus id. Hic
+            nesciunt quos commodi non voluptatibus ratione temporibus sint animi
+            at.
+          </p>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default Education;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero__container">
       <div className="hero__container-wrapper">
-        <div className="hero__container-text">
-          <h1>Paige Bendall</h1>
-          <h2>Web Developer</h2>
-          <div className="hero__container-text_buttons">
-            <button>Latest Projects</button>
-            <button>Contact Me</button>
-          </div>
-          <img src="#" alt="scroller" />
-        </div>
+        <motion.div
+          className="hero__container-text"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h1 variants={textVariants}>Paige Bendall</motion.h1>
+          <motion.h2 variants={textVariants}>Web Developer</motion.h2>
+          <motion.div
+            className="hero__container-text_buttons"
+            variants={textVariants}
+          >
+            <motion.button variants={textVariants}>
+              Latest Projects
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img src="#" alt="scroller" variants={textVariants} />
+        </motion.div>
       </div>
       <div className="hero__container-image">
         <img src="/src/assets/hero.png" alt="heroimage" />
